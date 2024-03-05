9184621edce90c483b1720db548c3be36e3ff04c
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-03-05 19:10:25 +0530

    level 1

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -4,24 +4,24 @@ import "./index.css";
 function App() {
   return (
     <div>
-      <h1>Smarter Tasks</h1>
-      <p className="text-3xl">
+      <h1 className="text-5xl  m-3 p-2  font-bold">Smarter Tasks</h1>
+      <p className="text-xl mx-3 p-2">
         <strong>Project:</strong>Graduation Final year Project(Revamp college
         Website)
       </p>
-      <div className="flex">
-        <div>
-          <h1>Pending</h1>
+      <div className="flex mx-10 my-3">
+        <div className="border-4 p-5 ml-10 border-black w-1/4 rounded ">
+          <h1 className="text-2xl m-2 font-bold ">Pending</h1>
           <TaskCard
             title="Build the website with static content"
-            date="12th April"
+            date="10th April" assinge="Rohit S" status="Due on"
           />
-          <TaskCard title="Add blog" date="12th April" />
+          <TaskCard title="Add blog" date="22th March" assinge="Rohit P" status="Due on" />
         </div>
-        <div>
-          <h1>Done</h1>
-          <TaskCard title="design the mockup " date="12th April" />
-          <TaskCard title="get approval from principal" date="12th April" />
+        <div  className="border-4 p-5 mx-10 border-black w-1/4 rounded " >
+          <h1 className="text-2xl m-2 font-bold ">Done</h1>
+          <TaskCard title="Design the mockup " date="10th April" assinge="Rohit M"  status="Completed on"/>
+          <TaskCard title="Get approval from principal" date="22th March" assinge="Ajay S" status="Completed on"  />
         </div>
       </div>
     </div>
--- hello-react/src/TaskCard.jsx
+++ hello-react/src/TaskCard.jsx
@@ -1,13 +1,12 @@
 import "./TaskCard.css";
 
-
 const TaskCard = (props) => {
   console.log(props);
   return (
     <div className="TaskItem">
-      <h2 className="text-3xl font-bold">{props.title}</h2>
-      <p >Completed on: due date...</p>
-      <p>Assignee: name...</p>
+      <h2 className="text-xl font-semibold"> {props.title} </h2>
+      <p>{props.status} : {props.date}</p>
+      <p>Assignee : {props.assinge}</p>
     </div>
   );
 };
