e97966d594336029fc01cec83bbcb1cf53180c42
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-03-19 19:56:42 +0530

    level 2

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,11 +1,12 @@
 import "./TaskCard.css";
 
 const TaskCard = (props) => {
-  console.log(props);
   return (
     <div className="TaskItem">
       <h2 className="text-xl font-semibold"> {props.title} </h2>
-      <p>{props.status} : {props.date}</p>
+      <p>
+        {props.status} : {props.date}
+      </p>
       <p>Assignee : {props.assinge}</p>
     </div>
   );
