6de6faa3ff42ae9867f7bbff8e1d11fb4f9d981e
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-04-06 22:19:41 +0530

    resloved routing issues

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -29,15 +29,15 @@ const router = createBrowserRouter([
     ),
     children: [
       {
-        path: "/home",
+        path: "home",
         element: <HomePage />,
       },
       {
-        path: "/tasks",
+        path: "tasks",
         element: <TaskListPage />,
       },
       {
-        path: "/tasks/:id",
+        path: "tasks/:id",
         element: <TaskDetailsPage />,
       },
     ],
