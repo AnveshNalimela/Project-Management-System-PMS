33d24472d7813ee6b9de972cd02b061d8c9b174c
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-04-10 10:02:28 +0530

    Resloved Routes Try-3

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -43,7 +43,7 @@ const router = createBrowserRouter([
   },
   { path: "/notfound", element: <Notfound /> },
 
-  
+  { path: "*", element: <Navigate to="/notfound" replace /> }
 ]);
 
 const App = () => {
