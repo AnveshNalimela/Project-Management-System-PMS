c8be2533625b5e06655eb9ec6e6adda847f0f8e0
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-04-10 13:34:14 +0530

    className text-base added to anocher tag

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -7,7 +7,7 @@ const Task = (props: TaskItem) => {
     <li className="flex w-fit">
       <div className="TaskItem  px-5 bg-white ">
         <div className="flex">
-          <a href={`/tasks/${props.id}`}>
+          <a className="text-base" href={`/tasks/${props.id}`}>
             <h3 className="text-base font-bold my-1">{props.title}</h3>
           </a>
         </div>
