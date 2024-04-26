974d9543a14be256e12589c06381875a17e6a9d2
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-04-26 22:06:11 +0530

    Making ready for Production

--- smarter-tasks/src/pages/projects/ProjectListItems.tsx
+++ smarter-tasks/src/pages/projects/ProjectListItems.tsx
@@ -6,6 +6,10 @@ export default function ProjectListItems() {
   const { projects, isLoading, isError, errorMessage } = state;
   console.log(projects);
 
+  // This check is to validate ErrorBoundary Implementation. Remove this before milestone submission
+  if (projects.length === 0) {
+    throw Error("Error!!!");
+  }
   if (projects.length === 0 && isLoading) {
     return <span>Loading...</span>;
   }
